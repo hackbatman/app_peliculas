a4dc0615bfc57cc9018921d5482cf9acbb156fd9
hackbatman <soybatman50417@gmail.com>
2022-06-23 13:39:14 -0600

    editado xd

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -7,13 +7,12 @@ const Movie = () => {
                 <h1>This is the list in movies favorites</h1>
                 <div className="mt-2">
                 <div className="card">
-                
                     <div className="card-title">
                         <h1>Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai</h1>
                     </div>
                     <div className="card-body">
                         <img className="card-img-top" src="https://somoskudasai.com/wp-content/uploads/2021/07/Visual-1-5-1447x2048.jpg" />
-                        <p>Futaba la cientifica investiga sobre los campos magneticos sobre las realidades alternas</p>
+                        <p>Futaba la cientifica investiga sobre los campos magneticos sobre las realidades alternas para llegar a un conclusion</p>
                     </div>
                     </div>
                 </div>
